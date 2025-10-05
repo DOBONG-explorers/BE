a732aec6df5fecd510e5ddcd1d3a4de238e1b504
hyo-lin <lin1137001@gmail.com>
2025-10-05 19:48:41 +0900

    feat: 리뷰 수정 request

---
+++ src/main/java/com/dobongzip/dobong/domain/map/dto/request/ReviewUpdateRequest.java
@@ -0,0 +1,8 @@
+package com.dobongzip.dobong.domain.map.dto.request;
+
+import jakarta.validation.constraints.*;
+
+public record ReviewUpdateRequest(
+        @NotNull @DecimalMin("0.5") @DecimalMax("5.0") Double rating,
+        @NotBlank @Size(max = 5000) String text
+) {}
